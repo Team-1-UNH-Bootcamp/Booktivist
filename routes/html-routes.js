3b1f1e60e7c77ee62d6b9d1d695deec28c4afa78
Ffej Caplan <ffej.caplan@gmail.com>
2020-06-30 19:37:04 -0400

    html routes added

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,41 @@
 const path = require('path');
 const router = require('express').Router();
 
+// home page
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// route to login page
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/login.html'));
+});
+
+// route to signup page
+router.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/signup.html'));
+});
+
+// route to about page
+router.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/about.html'));
+});
+
+// route to category page
+router.get('/categories', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// for admin login
+router.get('/admin/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// for admin review user book submissions
+router.get('/admin/review', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
