18602859f2eafa4e32921c94d8a6123e9f4576d0
Ffej Caplan <ffej.caplan@gmail.com>
2020-06-30 19:45:41 -0400

    added html pages to public folder and added additional html routes as needed

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -23,17 +23,27 @@ router.get('/about', (req, res) => {
 
 // route to category page
 router.get('/categories', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../public/categories.html'));
+});
+
+// route to my library
+router.get('/mylibrary', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/mylibrary.html'));
+});
+
+// route to my add book
+router.get('/addbook', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/addbook.html'));
 });
 
 // for admin login
 router.get('/admin/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../public/admin_login.html'));
 });
 
 // for admin review user book submissions
 router.get('/admin/review', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../public/admin_review.html'));
 });
 
 router.get('/', (req, res) => {
