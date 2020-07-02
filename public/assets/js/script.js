d49b663b17564bd3b56c19aea55d2b76f009333b
Ffej Caplan <ffej.caplan@gmail.com>
2020-07-02 19:31:47 -0400

    updated login html and lintrcjs

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,13 @@
 // populate book modal
 // GET /api/books/${bookid}
 // response -> populate book modal
+// give book modal id = to bookid
 
 // in book modal
 // if user click on plus sign (+) to add a book
 // into the mylibrary, check if user is logged in or not
 // if not redirect to signup page or login page
 // if yes then book will be added to user's my library
+
+// when plus is clicked and user is logged in
+// POST
