3bca5b7ece5becfd565c1a837041e3108f5eaf54
remi ibraheem <remicodesmuintu@gmail.com>
2020-07-06 15:20:02 -0400

    fixed lint issues

--- public/assets/js/addbook.js
+++ public/assets/js/addbook.js
@@ -12,80 +12,66 @@
 
 // $.ajax("/api/books", {type: 'POST', data: bookObj}).then(()=>{})
 // add that to the books table and set added to false
-var currentBook = null;
-$("#bookInfo").hide();
-$("#extraInfo").hide();
+const currentBook = null;
+$('#bookInfo').hide();
+$('#extraInfo').hide();
 
+function populateFields(book) {
+  $('#populateImg').attr('src', book.volumeInfo.imageLinks.thumbnail);
+  $('#populateBook').val(book.volumeInfo.title);
+  $('#populateAuthor').val(book.volumeInfo.authors.join(', '));
+  $('#populateDesc').val(book.volumeInfo.description);
+  $('#populateIllustrator').val(book.volumeInfo.thumbnail);
+  $('#populateDate').val(book.volumeInfo.publishedDate);
+}
 $('#isbnSubmit').click(() => {
-    var isbn = $('#isbnInput').val();
-    var isbn_without_hyphens = isbn.replace(/-/g, "");
-    var googleAPI = "https://www.googleapis.com/books/v1/volumes?q=" + isbn_without_hyphens;
-    $.getJSON(googleAPI, function(response) {
-        if (typeof response.items === "undefined") {
-            alert("No populatebooks match that ISBN.");
-        } else {
-            currentBook = response.items[0];
-            populateFields(response.items[0]);
-            console.log(currentBook);
+  const isbn = $('#isbnInput').val();
+  const isbnWithoutHyphens = isbn.replace(/-/g, '');
+  const googleAPI = `https://www.googleapis.com/books/v1/volumes?q=${isbnWithoutHyphens}`;
+  $.getJSON(googleAPI, (response) => {
+    if (typeof response.items === 'undefined') {
+      console.log('error ');
+    } else {
+      populateFields(response.items[0]);
+      console.log(currentBook);
 
-            $("#bookInfo").show();
-
-        }
-    });
+      $('#bookInfo').show();
+    }
+  });
 });
 
 $('#infoReject').click(() => {
-
-    $("#bookInfo").hide();
-    console.log(currentBook);
-
-
-
+  $('#bookInfo').hide();
+  console.log(currentBook);
 });
 $('#infoSubmit').click((e) => {
-    e.preventDefault()
-    $("#extraInfo").show();
-    console.log(currentBook);
-
+  e.preventDefault();
+  $('#extraInfo').show();
+  console.log(currentBook);
 });
 
 $('#submitBook').click((e) => {
-    e.preventDefault()
-    $("#extraInfo").show();
-    const category = $("#categorySelect").val()
-    const addYouTube = $("#addYouTube").val()
-    const textarea = $("#textarea").val()
-    console.log(category, addYouTube, textarea);
-    const payload = {
+  e.preventDefault();
+  $('#extraInfo').show();
+  const category = $('#categorySelect').val();
+  const addYouTube = $('#addYouTube').val();
+  const textarea = $('#textarea').val();
+  console.log(category, addYouTube, textarea);
+  const payload = {
 
-        thumbnail: currentBook.volumeInfo.imageLinks.thumbnail,
-        title: currentBook.volumeInfo.title,
-        authors: currentBook.volumeInfo.authors.join(", "),
-        description: currentBook.volumeInfo.description,
-        publishedDate: currentBook.volumeInfo.publishedDate,
-        category: category,
-        youTubeLink: addYouTube,
-        parentInsights: textarea
-    }
-    console.log(payload)
+    thumbnail: currentBook.volumeInfo.imageLinks.thumbnail,
+    title: currentBook.volumeInfo.title,
+    authors: currentBook.volumeInfo.authors.join(', '),
+    description: currentBook.volumeInfo.description,
+    publishedDate: currentBook.volumeInfo.publishedDate,
+    category,
+    youTubeLink: addYouTube,
+    parentInsights: textarea,
+  };
+  console.log(payload);
 
-    $.post("#", payload,
-        function(data, status) {
-            console.log(data, status);
-        }
-    );
+  $.post('#', payload,
+    (data, status) => {
+      console.log(data, status);
+    });
 });
-
-
-
-function populateFields(book) {
-
-    $("#populateImg").attr("src", book.volumeInfo.imageLinks.thumbnail)
-    $("#populateBook").val(book.volumeInfo.title)
-    $("#populateAuthor").val(book.volumeInfo.authors.join(", "))
-    $("#populateDesc").val(book.volumeInfo.description)
-    $("#populateIllustrator").val(book.volumeInfo.thumbnail)
-    $("#populateDate").val(book.volumeInfo.publishedDate)
-
-
-}
