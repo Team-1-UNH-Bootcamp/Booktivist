8d3b1ad1ebe7e895e1baf9961087be2b36fdec50
remi ibraheem <remicodesmuintu@gmail.com>
2020-07-12 21:53:18 -0400

    fixed lint

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -11,278 +11,285 @@ const BLM = ['category/3', 'BLM'];
 const LGBTQ = ['category/1', 'LGBTQ'];
 // const NAH = 'category/2';
 $(document).ready(() => {
-    loadBooks();
+  loadBooks();
 });
 
 const getBooks = (category) => {
-    return new Promise((resolve, reject) => {
-        $.ajax(`/api/books/${category[0]}`, { type: 'GET' })
-            .then((response) => {
-                response.forEach((data) => {
-                    let apiTitle = 'title';
-                    let apiId = 'id';
-                    let apiAuthor = 'author';
-                    let apiImg = 'image_link';
-                    if (category[0] !== 'recent') {
-                        apiTitle = 'books.title';
-                        apiId = 'books.id';
-                        apiAuthor = 'books.author';
-                        apiImg = 'books.image_link';
-                    }
+  return new Promise((resolve, reject) => {
+    $.ajax(`/api/books/${category[0]}`, { type: 'GET' })
+      .then((response) => {
+        response.forEach((data) => {
+          let apiTitle = 'title';
+          let apiId = 'id';
+          let apiAuthor = 'author';
+          let apiImg = 'image_link';
+          if (category[0] !== 'recent') {
+            apiTitle = 'books.title';
+            apiId = 'books.id';
+            apiAuthor = 'books.author';
+            apiImg = 'books.image_link';
+          }
 
 
-                    const parentDiv = $('<div>').attr({
-                        class: 'col-lg-3 col-sm-12 bookParentDiv',
-                    });
-                    const cardDiv = $('<div>').attr({ class: 'card bookCardDiv h-100' });
-                    const cardImg = $('<img>').attr({
-                        class: 'card-img-top bookCardImage h-70',
-                        src: data[apiImg],
-                        alt: 'Card Img Top',
-                    });
-                    const cardBody = $('<div>')
-                        .attr({ class: 'card-body bookCardBody h-30' })
-                        .css({
-                            paddingLeft: '5px',
-                            paddingRight: '5px',
-                            paddingBotton: '10px',
-                        });
-                    const linkTitle = $('<button>')
-                        .attr({ class: 'titleModal', value: data[apiId] })
-                        .attr('data-toggle', 'modal')
-                        .attr('data-target', '#bookModal')
-                        .css({ border: 'none', backgroundColor: 'white' });
-                    const bookTitle = $('<h3>')
-                        .attr({ class: 'card-title cardBookTitle' })
-                        .text(data[apiTitle]);
-                    const bookAuthor = $('<h4>')
-                        .attr({ class: 'card-subtitle cardBookAuthor' })
-                        .text(data[apiAuthor]);
-
-                    $(linkTitle).append(bookTitle, bookAuthor);
-                    $(cardBody).append(linkTitle);
-                    $(cardDiv).append(cardImg, cardBody);
-                    // cardImg,
-                    $(parentDiv).append(cardDiv);
-
-                    $(`#
-                                                        $ { category[1] }
-                                                        `).append(parentDiv);
-                    resolve(console.log('the data from the api', response));
-                });
-            })
-            .catch((err) => {
-                reject(err);
+          const parentDiv = $('<div>').attr({
+            class: 'col-lg-3 col-sm-12 bookParentDiv',
+          });
+          const cardDiv = $('<div>').attr({ class: 'card bookCardDiv h-100' });
+          const cardImg = $('<img>').attr({
+            class: 'card-img-top bookCardImage h-70',
+            src: data[apiImg],
+            alt: 'Card Img Top',
+          });
+          const cardBody = $('<div>')
+            .attr({ class: 'card-body bookCardBody h-30' })
+            .css({
+              paddingLeft: '5px',
+              paddingRight: '5px',
+              paddingBotton: '10px',
             });
-    });
+          const linkTitle = $('<button>')
+            .attr({ class: 'titleModal', value: data[apiId] })
+            .attr('data-toggle', 'modal')
+            .attr('data-target', '#bookModal')
+            .css({ border: 'none', backgroundColor: 'white' });
+          const bookTitle = $('<h3>')
+            .attr({ class: 'card-title cardBookTitle' })
+            .text(data[apiTitle]);
+          const bookAuthor = $('<h4>')
+            .attr({ class: 'card-subtitle cardBookAuthor' })
+            .text(data[apiAuthor]);
+
+          $(linkTitle).append(bookTitle, bookAuthor);
+          $(cardBody).append(linkTitle);
+          $(cardDiv).append(cardImg, cardBody);
+          // cardImg,
+          $(parentDiv).append(cardDiv);
+
+          $(`#${category[1]}`).append(parentDiv);
+          resolve(console.log('the data from the api', response));
+        });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
 
 const loadBooks = async() => {
-    await getBooks(recent);
-    await getBooks(BLM);
-    await getBooks(LGBTQ);
-    getModal();
+  await getBooks(recent);
+  await getBooks(BLM);
+  await getBooks(LGBTQ);
+  getModal();
 };
 
 
 $('#button-addon1').click(function(event) {
-    event.preventDefault();
-    const targetUrl = $('#searchBar')
-        .val()
-        .replace(/ /g, '&')
-        .toLowerCase();
-    window.location.href = ` / categories / ? search = $ { targetUrl }
-                                                        `;
+  event.preventDefault();
+  const targetUrl = $('#searchBar')
+    .val()
+    .replace(/ /g, '&')
+    .toLowerCase();
+  window.location.href = `/ categories / ? search = ${
+    targetUrl}`;
 });
 
 const getModal = () => {
-    console.log('the call');
-    $('.titleModal').click(function() {
-        console.log('the modal');
-        $('.modal-content').text('');
-        const index = this.value;
-        $.ajax('api/user_data', { type: 'GET' }).then((userData) => {
-            if (userData.message === false) {
-                notLoggedIn();
-            } else {
-                $.ajax(`api/mylibrary/${index}`, { type: 'GET' }).then((isAdded) => {
-                    if (isAdded === null) {
-                        console.log('notAdded');
-                        addBookToLibrary(index);
-                    } else {
-                        console.log('added');
-                        removeBookFromLibrary(index);
-                    }
-                });
-                fillModal(index);
-            };
+  console.log('the call');
+  $('.titleModal').click(function() {
+    console.log('the modal');
+    $('.modal-content').text('');
+    const index = this.value;
+    $.ajax('api/user_data', { type: 'GET' }).then((userData) => {
+      if (userData.message === false) {
+        notLoggedIn();
+      } else {
+        $.ajax(`
+            api / mylibrary / $ { index }
+            `, { type: 'GET' }).then((isAdded) => {
+          if (isAdded === null) {
+            console.log('notAdded');
+            addBookToLibrary(index);
+          } else {
+            console.log('added');
+            removeBookFromLibrary(index);
+          }
+        });
+        fillModal(index);
+      }
+    });
+
+    const notLoggedIn = () => {
+      console.log('not logged in');
+      addPlusSign();
+      $('#thePlusSign').click(() => {
+        const topDiv = $('<div>')
+          .attr({ class: 'alert alert-danger notLoggedIn' })
+          .text('Please Login To Add This Book To Your Library');
+        $('.modal-content').prepend(topDiv);
+        setTimeout(() => {
+          topDiv.remove();
+        }, 2000);
+      });
+    };
+
+    const addBookToLibrary = (bookId) => {
+      addPlusSign();
+      plusMinusBtn(bookId);
+    };
+
+    const removeBookFromLibrary = (bookId) => {
+      addMinusSign();
+      plusMinusBtn(bookId);
+    };
+
+    const plusMinusBtn = (bookId) => {
+      $('#theMinusSign').click(() => {
+        $.ajax(`/api/mylibrary/${bookId}`, {
+          type: 'DELETE '
+        }).then(() => {
+          $('#theMinusSign').remove();
+          addPlusSign();
+        });
+      });
+      $('#thePlusSign').click(() => {
+        $.ajax(`/api/mylibrary/${bookId}`, {
+          type: 'POST '
+        }).then(() => {
+          $('#thePlusSign').remove();
+          addMinusSign();
+        });
+      });
+    };
+
+    const addPlusSign = () => {
+      const plusSign = $('<i>').attr({
+        class: 'fa fa-plus plusSign',
+        id: 'plusSign',
+      });
+
+      const addButton = $('<button>')
+        .attr({
+          class: 'btn btn-primary addBookBtn',
+          id: 'thePlusSign',
+        })
+        .css({
+          position: 'float-left',
+          width: '35px',
+          height: '35px',
+          marginTop: '10px',
+
         });
 
-        const notLoggedIn = () => {
-            console.log('not logged in');
-            addPlusSign();
-            $('#thePlusSign').click(() => {
-                const topDiv = $('<div>')
-                    .attr({ class: 'alert alert-danger notLoggedIn' })
-                    .text('Please Login To Add This Book To Your Library');
-                $('.modal-content').prepend(topDiv);
-                setTimeout(() => {
-                    topDiv.remove();
-                }, 2000);
-            });
-        };
+      $(addButton).append(plusSign);
+      $('.modal-content').prepend(addButton);
+    };
 
-        const addBookToLibrary = (bookId) => {
-            addPlusSign();
-            plusMinusBtn(bookId);
-        };
+    const addMinusSign = () => {
+      const minusSign = $('<i>').attr({
+        class: 'fa fa-minus minusSign',
+        id: 'minusSign',
+      });
 
-        const removeBookFromLibrary = (bookId) => {
-            addMinusSign();
-            plusMinusBtn(bookId);
-        };
+      const addButton = $('<button>')
+        .attr({
+          class: 'btn btn-primary addBookBtn',
+          id: 'theMinusSign',
+        })
+        .css({
+          position: 'float-left',
+          width: '35px',
+          height: '35px',
+          marginTop: '10px',
 
-        const plusMinusBtn = (bookId) => {
-            $('#theMinusSign').click(() => {
-                $.ajax(` / api / mylibrary / $ { bookId }
-                                                        `, { type: 'DELETE' }).then(() => {
-                    $('#theMinusSign').remove();
-                    addPlusSign();
-                });
-            });
-            $('#thePlusSign').click(() => {
-                $.ajax(` / api / mylibrary / $ { bookId }
-                                                        `, { type: 'POST' }).then(() => {
-                    $('#thePlusSign').remove();
-                    addMinusSign();
-                });
-            });
-        };
-
-        const addPlusSign = () => {
-            const plusSign = $('<i>').attr({
-                class: 'fa fa-plus plusSign',
-                id: 'plusSign',
-            });
-
-            const addButton = $('<button>')
-                .attr({
-                    class: 'btn btn-primary addBookBtn',
-                    id: 'thePlusSign',
-                })
-                .css({
-                    position: 'float-left',
-                    width: '35px',
-                    height: '35px',
-                    marginTop: '10px',
-
-                });
-
-            $(addButton).append(plusSign);
-            $('.modal-content').prepend(addButton);
-        };
-
-        const addMinusSign = () => {
-            const minusSign = $('<i>').attr({
-                class: 'fa fa-minus minusSign',
-                id: 'minusSign',
-            });
-
-            const addButton = $('<button>')
-                .attr({
-                    class: 'btn btn-primary addBookBtn',
-                    id: 'theMinusSign',
-                })
-                .css({
-                    position: 'float-left',
-                    width: '35px',
-                    height: '35px',
-                    marginTop: '10px',
-
-                });
+        });
 
 
-            $(addButton).append(minusSign);
-            $('.modal-content').prepend(addButton);
-        };
+      $(addButton).append(minusSign);
+      $('.modal-content').prepend(addButton);
+    };
 
-        const fillModal = (i) => {
-
-            $.ajax(`/api/book/${i}`, { type: 'GET' }).then((response) => {
-                const bookJpg = $('<img>').attr({
-                    src: response.image_link,
-                    class: 'col-sm-4 modalCoverJpg bookInfoChild',
-                });
-                const bookTitle = $('<h5>')
-                    .attr({ class: 'modalBookTitle bookInfoChild' })
-                    .text(
-                        // eslint-disable-next-line comma-dangle
-                        `TITLE: ${response.title}`
-                    );
-                const bookSubtitle = $('<h6>')
-                    .attr({ class: 'modalBooksubTitle bookInfoChild' })
-                    .text('');
-                const bookAuthor = $('<h6>')
-                    .attr({ class: 'modalBookAuthor bookInfoChild' })
-                    .text(
-                        // eslint-disable-next-line comma-dangle
-                        `AUTHOR: ${response.author}`
-                    );
-                const bookIllustrator = $('<h6>')
-                    .attr({ class: 'modalBookIllustrator bookInfoChild' })
-                    .text(`illustrator: ${response.illustrator}`);
-                const bookDescriptionHeader = $('<h6>')
-                    .attr({ class: 'descriptionHeader extBookChild' })
-                    .text('Description:');
-                const bookDescription = $('<p>')
-                    .text(response.description)
-                    .attr({ id: 'bookDesc extBookChild' });
-                const keyPointsHeader = $('<h6>')
-                    .text('Key Discussion Points')
-                    .attr({ class: 'keyPointsHeader extBookChild' });
-                const keyPoints = $('<p>')
-                    .text('')
-                    .attr({ class: 'keyPoints extBookChild' });
-                const youTubeLink = response.youtube_link;
-                let youTubeAppend = '';
-                if (youTubeLink !== null) {
-                    youTubeAppend = $('<a>')
-                        .attr({ href: youTubeLink, class: 'youTubeLink extBookChild' })
-                        .text('Watch This Book Being Read on YouTube');
-                }
-                const pubDate = $('<p>')
-                    .text(response.pub_date)
-                    .attr({ class: 'pubDate extBookChild' });
-                const isbn = $('<p>')
-                    .text(response.isbn)
-                    .attr({ class: 'isbn extBookChild' });
-
-                const rowDiv = $('<div>')
-                    .attr({ class: 'row rowDiv' })
-                    .css({ marginTop: '80px' });
-                const bookInfo = $('<div>').attr({ class: 'col-sm-6 modalBookInfo' });
-                const extendedBookInfo = $('<div>').attr({ class: 'extendedInfo' });
-                // const bookCategories = $('<ul>');
-                // const allCategories = response.categories
-                // allCategories.forEach((cat)=>{
-                // const bookCategory = $('<li>').text(cat);
-                // $(bookCategories).append(bookCategory)
-                // })
-                $(bookInfo).append(bookTitle, bookSubtitle, bookAuthor, bookIllustrator);
-                $(rowDiv).append(bookJpg, bookInfo);
-                $(extendedBookInfo).append(
-                    bookDescriptionHeader,
-                    bookDescription,
-                    keyPointsHeader,
-                    keyPoints,
-                    youTubeAppend,
-                    // bookCategories,
-                    pubDate,
-                    // eslint-disable-next-line comma-dangle
-                    isbn
-                );
-                $('.modal-content').append(rowDiv, extendedBookInfo);
-            });
+    const fillModal = (i) => {
+      $.ajax(`/api/book/${i}`, { type: 'GET' }).then((response) => {
+        const bookJpg = $('<img>').attr({
+          src: response.image_link,
+          class: 'col-sm-4 modalCoverJpg bookInfoChild',
+        });
+        const bookTitle = $('<h5>')
+          .attr({ class: 'modalBookTitle bookInfoChild' })
+          .text(
+            // eslint-disable-next-line comma-dangle
+            `
+                                    TITLE: $ { response.title }
+                                    `
+          );
+        const bookSubtitle = $('<h6>')
+          .attr({ class: 'modalBooksubTitle bookInfoChild' })
+          .text('');
+        const bookAuthor = $('<h6>')
+          .attr({ class: 'modalBookAuthor bookInfoChild' })
+          .text(
+            // eslint-disable-next-line comma-dangle
+            `
+                                    AUTHOR: $ { response.author }
+                                    `
+          );
+        const bookIllustrator = $('<h6>')
+          .attr({ class: 'modalBookIllustrator bookInfoChild' })
+          .text(`
+                                    illustrator: $ { response.illustrator }
+                                    `);
+        const bookDescriptionHeader = $('<h6>')
+          .attr({ class: 'descriptionHeader extBookChild' })
+          .text('Description:');
+        const bookDescription = $('<p>')
+          .text(response.description)
+          .attr({ id: 'bookDesc extBookChild' });
+        const keyPointsHeader = $('<h6>')
+          .text('Key Discussion Points')
+          .attr({ class: 'keyPointsHeader extBookChild' });
+        const keyPoints = $('<p>')
+          .text('')
+          .attr({ class: 'keyPoints extBookChild' });
+        const youTubeLink = response.youtube_link;
+        let youTubeAppend = '';
+        if (youTubeLink !== null) {
+          youTubeAppend = $('<a>')
+            .attr({ href: youTubeLink, class: 'youTubeLink extBookChild' })
+            .text('Watch This Book Being Read on YouTube');
         }
-    });
-}
+        const pubDate = $('<p>')
+          .text(response.pub_date)
+          .attr({ class: 'pubDate extBookChild' });
+        const isbn = $('<p>')
+          .text(response.isbn)
+          .attr({ class: 'isbn extBookChild' });
+
+        const rowDiv = $('<div>')
+          .attr({ class: 'row rowDiv' })
+          .css({ marginTop: '80px' });
+        const bookInfo = $('<div>').attr({ class: 'col-sm-6 modalBookInfo' });
+        const extendedBookInfo = $('<div>').attr({ class: 'extendedInfo' });
+        // const bookCategories = $('<ul>');
+        // const allCategories = response.categories
+        // allCategories.forEach((cat)=>{
+        // const bookCategory = $('<li>').text(cat);
+        // $(bookCategories).append(bookCategory)
+        // })
+        $(bookInfo).append(bookTitle, bookSubtitle, bookAuthor, bookIllustrator);
+        $(rowDiv).append(bookJpg, bookInfo);
+        $(extendedBookInfo).append(
+          bookDescriptionHeader,
+          bookDescription,
+          keyPointsHeader,
+          keyPoints,
+          youTubeAppend,
+          // bookCategories,
+          pubDate,
+          // eslint-disable-next-line comma-dangle
+          isbn
+        );
+        $('.modal-content').append(rowDiv, extendedBookInfo);
+      });
+    };
+  });
+};
