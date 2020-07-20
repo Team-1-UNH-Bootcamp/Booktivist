8529eddc8dcb39c869fd8975ebafc054c473693c
Pri_030 <priyankasingh30@gmail.com>
2020-07-20 19:17:34 -0400

    updated config.js

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    use_env_variable: 'mysql://l7gedza68cytiys4:ktl1lii1njqm5yie@e11wl4mksauxgu1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qj424b6aqafk29se',
+    use_env_variable: 'JAWSDB_URL',
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
